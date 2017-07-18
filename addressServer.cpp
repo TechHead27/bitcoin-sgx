67a71db88d6955144d32240c2fd8722f8bfde29c
Cody Hunt <crhunt@calpoly.edu>
2017-07-18 16:49:22 -0700

    Added interrupt handler and wallet export

--- addressServer.cpp
+++ addressServer.cpp
@@ -1,7 +1,9 @@
 #include <string>
 #include <iostream>
+#include <fstream>
 #include <random>
 #include <chrono>
+#include <signal.h>
 #include <bitcoin/bitcoin.hpp>
 #include <bitcoin/client.hpp>
 #include <zmq.hpp>
@@ -9,6 +11,23 @@
 #include "wallet.h"
 #include "addressServer.h"
 using namespace bc;
+
+bool interrupted = false;
+
+void signal_handler(int signal)
+{
+    interrupted = true;
+}
+
+void catch_signals()
+{
+    struct sigaction action;
+    action.sa_handler = signal_handler;
+    action.sa_flags = 0;
+    sigemptyset(&action.sa_mask);
+    sigaction(SIGINT, &action, NULL);
+    sigaction(SIGTERM, &action, NULL);
+}
 
 std::mt19937 *seedRandom()
 {
@@ -23,10 +42,17 @@ void startServer(Wallet *wallet)
     zmq::socket_t socket(context, ZMQ_REP);
     socket.bind("tcp://*:" + std::to_string(port));
 
-    while (true) {
+    while (!interrupted) {
         zmq::message_t request;
 
-        socket.recv(&request);
+        try
+        {
+            socket.recv(&request);
+        } catch (zmq::error_t e)
+        {
+            std::cerr << "recv interrupted" << std::endl;
+            return;
+        }
         std::cerr << "Recieved request" << std::endl;
 
         // Send random child address as string
@@ -43,10 +69,15 @@ int main(int argc, char *argv[])
 {
     Wallet *wallet;
 
+    catch_signals();
+
     if (argc < 2)
     {
         std::cout << "Creating new wallet" << std::endl;
         wallet = new Wallet();
+        std::cout << "Writing wallet to wallet.out" << std::endl;
+        std::ofstream outFile("wallet.out");
+        wallet->displayMnemonic(outFile);
     } else
     {
         std::cout << "Loading wallet from " << argv[1] << std::endl;
